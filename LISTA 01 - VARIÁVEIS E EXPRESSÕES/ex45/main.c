be00e0d40184c4777b51da8ffdb1349f69025718
Jonathan Amancio <amanciio93@gmail.com>
2026-06-22 22:04:14 -0300

    ex45

---
+++ LISTA 01 - VARIÁVEIS E EXPRESSÕES/ex45/main.c
@@ -0,0 +1,25 @@
+/*
+ * Arquivo   : main.c
+ * Autor     : Jonathan Amancio
+ * Data      : 18/06/2026 - 18:51
+ * Descrição : 45. Faça um programa para converter uma letra maiúscula em letra minúscula. Use a tabela
+                   ASCII para resolver o problema.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char letraMinuscula, letraMaiuscula;
+
+    printf("Informe uma letra MAIUSCULA: ");
+    scanf("%c", &letraMaiuscula);
+
+    //Somando 32 ao valor ASCII da letra maiuscula, encontramos a letra minuscula, 32 posições a frente;
+    letraMinuscula = letraMaiuscula + 32;
+
+    printf("Letra minuscula: %c.", letraMinuscula);
+
+    return 0;
+}
